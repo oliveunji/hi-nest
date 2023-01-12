67a6e7817637deced251e27557e2b515e29c7031
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-01-13 08:43:09 +0900

    DTO 생성, 모듈화, DI 적용

---
+++ Learnings.md
@@ -0,0 +1,9 @@
+# 2023-01-13
+
+- mapped-types 패키지 설치 이유: transform your types and mapped types build by nest js developers
+
+- nest g mo: 모듈 생성하는 cli, App js에 있던 서비스와 컨트롤러를 -> movies 모듈 생성하고 -> 그 안으로 집어넣어서 코드 깔끔하게 함
+
+- nest g co: 컨트롤러 생성하는 cli, App 레벨의 컨트롤러를 생성
+
+- nest.js 는 express 기반으로 만들어졌지만, 이거를 fastify 기반으로 바꿀 수 있음. 다만 코드 구현을 express에서만 동작하는 함수로 작성하면 나중에 바꾸기 어렵기 때문에 nest js기반 표현식을 써서 만드는게 좋다.
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,8 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -25,22 +27,23 @@ export class MoviesController {
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
     return this.movieService.getOne(movieId);
   }
 
   @Post()
-  createMovie(@Body() movieData) {
+  createMovie(@Body() movieData: CreateMovieDto) {
     return this.movieService.create(movieData);
   }
 
   @Delete('/:id')
-  deleteMovie(@Param('id') movieId: string) {
+  deleteMovie(@Param('id') movieId: number) {
     return this.movieService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.movieService.update(movieId, updateData);
   }
 }
